92c4db47bf9c5990079c76659b9de0a97ac811cb
Steffen Brinckmann <sbrinckm@gmail.com>
2024-01-15 11:26:20 +0100

    try 2

--- tests/test_99_logging.py
+++ tests/test_99_logging.py
@@ -1,26 +1,36 @@
+#!/usr/bin/python3
 """ Convert a logging.json to a readme file """
 from pathlib import Path
 import json
+import unittest
 
-columns = ['params_metadata_json', 'pypi_rocrate']
-header  = """
-## Results of verification
-automatically created
+class Test_2(unittest.TestCase):
+    """
+    derived class for this test
+    """
+    def test_main(self):
+        """
+        main function
+        """
+        columns = ['params_metadata_json', 'pypi_rocrate']
+        header  = """
+        ## Results of verification
+        automatically created
 
-"""
+        """
 
-if Path('tests/logging.json').exists():
-    logJson = json.load(open('tests/logging.json'))
-    output = open('tests/logging.md', 'w')
-    output.write(header)
-    output.write(f'| software | file name | {" | ".join(columns)} |\n')
-    output.write(f'| -------- | --------- | {" | ".join(["-----------" for _ in columns])} |\n')
-    for filename, result in logJson.items():
-        software = filename.split('/')[2]
-        indFileName = filename.split('/')[3]
-        resultStr   = ' | '.join([':white_check_mark:' if result[col] else ':x:' for col in columns])
-        output.write(f'| {software} | {indFileName} | {resultStr} |\n')
-    output.close()
-    print('Created logging markdown')
-else:
-    print('Did not create logging markdown')
+        if Path('tests/logging.json').exists():
+            logJson = json.load(open('tests/logging.json'))
+            output = open('tests/logging.md', 'w')
+            output.write(header)
+            output.write(f'| software | file name | {" | ".join(columns)} |\n')
+            output.write(f'| -------- | --------- | {" | ".join(["-----------" for _ in columns])} |\n')
+            for filename, result in logJson.items():
+                software = filename.split('/')[2]
+                indFileName = filename.split('/')[3]
+                resultStr   = ' | '.join([':white_check_mark:' if result[col] else ':x:' for col in columns])
+                output.write(f'| {software} | {indFileName} | {resultStr} |\n')
+            output.close()
+            print('Created logging markdown')
+        else:
+            print('Did not create logging markdown')
